db03492842422eb7d75a19957ca35f1d4792c47b
tsunlamchan <tsunlamc@nycstudents.net>
2026-09-22 15:18:54 +0000

    5

---
+++ Lesson-05 Activity/Main.java
@@ -0,0 +1,121 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+    
+
+/*  
+    Challenge 1:
+    Create two integer variables and Assign values to them. 
+    Calculate the sum of the two numbers and store the 
+    calculated sum and then display it.
+    
+*/
+    int x1 = 5;
+    int x2 = 10;
+    int sum = x1 + x2;
+    System.out.println(sum);
+   
+
+
+/*  
+    Challenge 2:
+    Create three variables to assign three grades and Assign values to each grade. 
+    Calculate the sum of the three grades and store the 
+    calculated sum and then display it.
+    
+*/
+    float grade1 = 65;
+    float grade2 = 95;
+    float grade3 = 82;
+    float grade_sum = grade1 + grade2 + grade3;
+    System.out.println(grade_sum);
+
+
+
+
+/*  
+    Challenge 3:
+    Calculate the average from the three grades from challenge 2,
+    store the value and then display it.
+    Declare and assign values to any new variables
+    NOTE: Does it look correct, check with a calculator?
+*/
+    double grade_avg = ((grade1 + grade2 + grade3)/300.0) * 100;
+    System.out.println(grade_avg);
+
+
+/*  
+    Challenge 4:
+    Write the following equation in EQ1.PNG file in Java; store the result and the display it:
+    Declare and assign values to any new variables
+
+*/  
+    double A = 10;
+    double x = 5;
+    double y = A / (x + 1.0);
+    System.out.println(y);
+
+  
+
+/*  
+    Challenge 5:
+    Using the variables same variables from challenge4 above, write the following equation in EQ2.PNG file in Java, store the result and the display it:
+
+    Declare and assign values to any new variables
+
+*/ 
+    double Y = ( (2 * x) * ( x + 1 ) * (-x / 2 )) / A;
+    System.out.println(Y);
+ 
+
+
+
+
+
+/*  
+    Challenge 6:
+    Create the variables and write the equation in
+    file  EQ3.PNG
+
+    Declare and assign values to any new variables
+*/
+    double b = 10;
+    double h = 20;
+    double area  = (1/2.0)*b*h;
+    System.out.println(area);
+
+
+
+
+
+/*  
+    **** Bonus Challenge ****:
+    Create a variable that stores the total number of eggs 
+    and assign it 100. We want to fill as many baskets with 
+    eggs as we can. Each basket can hold only 12 eggs.
+
+    1) Write the java code that will calcute how many baskets
+    of 12 eggs can we fill fully.
+
+    HINT: What do we get when we divide an integer by 
+    an integer in Java
+
+    2) Write the java code that will calculate how many eggs
+    are left over after we filled as many baskets of 12 eggs.
+*/
+    double eggs = 100
+    double baskets = 100 / 12;
+    double remainder = 100%12
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
